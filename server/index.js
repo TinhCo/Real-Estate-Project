7f7f24cc651ba5d8ae829adec7cb3722c66801ec
TinhCo <phungtoan872@gmail.com>
2023-11-18 15:21:37 +0700

    video 4

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 require('dotenv').config()
 const cors = require('cors')
+const dbconn = require('./config/dbcconn')
 const app = express()
 app.use(cors({
     origin: process.env.CLIENT_URL
 }))
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true })) 
+
+dbconn();
 
 const PORT = process.env.PORT || 7777
-app.listen(5000, () => console.log(':::::::::SERVER REATY ON' + PORT))
+app.listen(5000, () => console.log(':::::::::SERVER REATY ON ' + PORT))
